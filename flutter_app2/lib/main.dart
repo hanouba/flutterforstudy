4a10544e3cf6e53c30b8a6d8ea3be1ee05f82e30
HanSir <han_belief@163.com>
2021-04-02 10:26:05 +0800

    listview 使用

--- flutter_app2/lib/main.dart
+++ flutter_app2/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -9,35 +11,62 @@ class MyApp extends StatelessWidget {
     final wordPair = new WordPair.random();
     return MaterialApp(
       title: "你好这是flutter",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("app bar title"),
-        ),
-        body: new Center(
-          // child: new Text("body child"),
-          child: new RandWords(),
-        ),
-
-      ),
+      home: new RandWords()
     );
   }
 }
+
 class RandWords extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return new RandWordsState();
   }
-
-
 }
 
 class RandWordsState extends State<RandWords> {
+
+  //字母列表保存
+  final _suggestions = <WordPair>[];
+  //字母大小控制
+  final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    final wordPair  = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+    // final wordPair = new WordPair.random();
+    // return new Text(wordPair.asPascalCase);
+
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("listview "),
+        ),
+        body: _buildSuggestions(),
+      );
   }
 
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          // 在每一列之前，添加一个1像素高的分隔线widget
+          if(i.isOdd) return new Divider();
+          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+          final index = i ~/2 ;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+
+  }
+//样式
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
 }
